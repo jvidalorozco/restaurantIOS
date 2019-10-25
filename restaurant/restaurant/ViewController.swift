0273f5aa804ac70771587a1e98646fac9d565966
Jorge V <jorgevidal1995@hotmail.com>
2019-10-24 23:08:22 -0500

    First commit

---
+++ restaurant/restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  restaurant
+//
+//  Created by MacBook Pro on 10/24/19.
+//  Copyright © 2019 Celeste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
